55542ec92eb28e4c81e9a552c1db2598005e41ef
Ben Tu <bentu1991@gmail.com>
2021-08-10 15:06:10 -0700

    add function

--- product.py
+++ product.py
@@ -1,36 +1,50 @@
 import os
 
-#check if os has file
-product = []
-if os.path.isfile('product.csv'):
-	print('found the file!')
-	with open('product.csv', 'r', encoding='utf-8') as f:
-		for line in f:
-			if 'Product, Price' in line:
-				continue
-			name, price = line.strip().split(',')
-			product.append([name, price])
-	print(product)
-
-else:
-	print('lol you fucked up')
+def read_file(filename):
+	product = []
+	with open(filename, 'r', encoding='utf-8') as f:
+			for line in f:
+				if 'Product, Price' in line:
+					continue
+				name, price = line.strip().split(',')
+				product.append([name, price])
+	return product
 
 #let user input the data
-while True:
-	name = input('please enter the merchandise name: ')
-	if name == 'q':
-		break
-	price = input('please enter the cost for merchandise: ')
-	prine = int(price)
-	p = [name, price]
-	product.append(p)
+def user_input(product):
+	while True:
+		name = input('please enter the merchandise name: ')
+		if name == 'q':
+			break
+		price = input('please enter the cost for merchandise: ')
+		price = int(price)
+		product.append(name, price)
+	print(product)
+	return product
 
 #print out the result
-for p in product: 
-	print(p[0], 'price are', p[1])
+def print_products(product):
+	for p in product: 
+		print(p[0], 'price are', p[1])
 
 #overwrite the file
-with open('product.csv', 'w', encoding='utf-8') as f:
-	f.write('Product, Price\n')
-	for p in product:
-		f.write(p[0] + ',' + str(p[1]) + '\n')
+def over_write(filename, product):
+	with open(filename, 'w', encoding='utf-8') as f:
+		f.write('Product, Price\n')
+		for p in product:
+			f.write(p[0] + ',' + str(p[1]) + '\n')
+
+def main():
+	filename = 'product.csv'
+	if os.path.isfile(filename):
+		print('found the file ')
+		product = read_file(filename)
+	else:
+		print('lol you fucked')
+
+	product = user_input(product)
+	print_products(product)
+	over_write('product.csv', product)
+
+main()
+
