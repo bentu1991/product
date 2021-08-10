ecb5082acacb8141c0fb0b7f1c2e29009768b0a7
Ben Tu <bentu1991@gmail.com>
2021-08-10 11:33:58 -0700

    add check file list

--- product.py
+++ product.py
@@ -1,14 +1,19 @@
+import os
+
+#check if os has file
 product = []
+if os.path.isfile('product.csv'):
+	print('found the file!')
+	with open('product.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if 'Product, Price' in line:
+				continue
+			name, price = line.strip().split(',')
+			product.append([name, price])
+	print(product)
 
-#open file and read
-with open('product.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if 'Product, Price' in line:
-			continue
-		name, price = line.strip().split(',')
-		product.append([name, price])
-
-print(product)
+else:
+	print('lol you fucked up')
 
 #let user input the data
 while True:
